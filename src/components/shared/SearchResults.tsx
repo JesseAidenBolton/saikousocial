cb8e18b6c793951392ff1b6d3e37bc4671a81b14
JesseAidenBolton <jesseabolton@gmail.com>
2023-12-14 00:21:02 +1100

    added explore page, search, infinite scroll

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,26 @@
+import {Models} from "appwrite";
+import {Loader2} from "lucide-react";
+import GridPostList from "@/components/shared/GridPostList.tsx";
+
+
+type SearchResultProps = {
+    isSearchFetching: boolean;
+    searchedPosts: Models.Document[]
+}
+
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+    
+    if(isSearchFetching) return <Loader2 className="animate-spin"/>
+    
+    if(searchedPosts && searchedPosts.documents.length > 0) {
+        return (
+            <GridPostList posts={searchedPosts.documents} />
+        )
+    }
+    
+    return (
+        <p className="text-light-4 mt-10 text-center w-full">No results found.</p>
+    );
+};
+
+export default SearchResults;
